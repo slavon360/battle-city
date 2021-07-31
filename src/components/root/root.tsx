511dcb93a030dfd54bd9d046dbedf48c759d2ada
slavon360 <slavon360@github.com>
2021-08-01 00:49:30 +0300

    tank movement + collisions logic

--- src/components/root/root.tsx
+++ src/components/root/root.tsx
@@ -38,14 +38,14 @@ interface stepAxisRef {
 const sprite = new Sprite(sprite_url);
 
 export const Root = () => {
-	const [tank_coordinates, setTankCoordinates] = useState({ tank_x: 50, tank_y: 60 });
+	const [tank_coordinates, setTankCoordinates] = useState({ tank_x: 0, tank_y: 60 });
 	const { tank_x, tank_y } = tank_coordinates;
 	const active_keys: activeKeys = useRef(null);
 	const current_animation_frame: any = useRef(null);
 	const canvas_ref: any = useRef(null);
 	const tank_img_coordinates_ref: imgCoordinatesRef = useRef({ img_x: 0, img_y: 0 });
 	const step_axis_ref: stepAxisRef = useRef({ step_x: 0, step_y: 0 });
-	// const { _canvas } = useContext(canvasContext);
+	// const flattened_grid_ref: any = useRef(null);
 	
 	const initGameboardLevels = useCallback(() => {
 		const canvas = canvas_ref.current;
@@ -63,6 +63,79 @@ export const Root = () => {
 			});
 		})
 	}, []);
+
+	const canNotMove = (direction: string, tank_x: number, tank_y: number ) => {
+		// let cant = false;
+		return GRID_ELEMENTS_LEVEL1.find((row, row_index) => {
+			return row.find((cell, cell_index) => {
+				const [sprite_x, sprite_y, sprite_width, sprite_height] = SPRITES_ELEMENTS[cell];
+				const element_x_begin = cell_index * sprite_width;
+				const element_x_end = element_x_begin + sprite_width;
+				const element_y_begin = row_index * sprite_height;
+				const element_y_end = element_y_begin + sprite_height;
+				const tank_x_end = tank_x + sprite_element_width;
+				const tank_y_end = tank_y + sprite_element_width;
+				
+				switch (direction) {
+					case 'ArrowUp':
+						if (
+							cell > 0 && tank_y === element_y_end &&
+							(tank_x >= element_x_begin && tank_x_end >= element_x_end &&
+							tank_x - element_x_begin < sprite_element_width) ||
+							cell > 0 && tank_y === element_y_end &&
+							(tank_x <= element_x_begin && tank_x_end <= element_x_end &&
+							element_x_end - tank_x_end < sprite_element_width) 
+						) {
+							console.log(tank_x, element_x_begin, tank_x_end, element_x_end);
+							return true;
+						}
+						return false;
+					case 'ArrowDown':
+						if (
+							cell > 0 && tank_y_end === element_y_begin &&
+							(tank_x >= element_x_begin && tank_x_end >= element_x_end &&
+							tank_x - element_x_begin < sprite_element_width) ||
+							cell > 0 && tank_y_end === element_y_begin &&
+							(tank_x <= element_x_begin && tank_x_end <= element_x_end &&
+							element_x_end - tank_x_end < sprite_element_width) 
+						) {
+							return true;
+						}
+						return false;
+					case 'ArrowLeft':
+						if (
+							cell > 0 && tank_x === element_x_end &&
+							(tank_y_end >= element_y_end &&
+							tank_y_end - element_y_end >= 0 && tank_y_end - element_y_end < sprite_element_width) ||
+							cell > 0 && tank_x === element_x_end &&
+							(tank_y_end <= element_y_end &&
+							(tank_y - element_y_begin >= 0 && tank_y - element_y_begin < sprite_element_width) ||
+							element_y_begin - tank_y  >= 0 && element_y_begin - tank_y < sprite_element_width)
+						) {
+							return true;
+						}
+						return false;
+					case 'ArrowRight':
+						if (
+							cell > 0 && tank_x_end === element_x_begin &&
+							(tank_y_end >= element_y_end &&
+							tank_y_end - element_y_end >= 0 && tank_y_end - element_y_end < sprite_element_width) ||
+							cell > 0 && tank_x_end === element_x_begin &&
+							(tank_y_end <= element_y_end &&
+							(tank_y - element_y_begin >= 0 && tank_y - element_y_begin < sprite_element_width) ||
+							element_y_begin - tank_y  >= 0 && element_y_begin - tank_y < sprite_element_width)
+						) {
+							return true;
+						}
+						return false;
+
+					default: return false;
+				}
+			});
+		});
+		// console.log(cant);
+		// return cant;
+	}
 
 	const handleTankImgCoordinates = (key: string) => {
 		switch (key) {
@@ -116,8 +189,15 @@ export const Root = () => {
 				setTankCoordinates(({ tank_x, tank_y }) => {
 					const dead_end = tank_y <= 0;
 
-					if (dead_end) {
+					if (dead_end || canNotMove(code, tank_x, tank_y)) {
 						renderTank(sprite, { tank_x, tank_y });
+					}
+
+					if (canNotMove(code, tank_x, tank_y)) {
+						return {
+							tank_x: tank_x,
+							tank_y: tank_y
+						}	
 					}
 
 					return {
@@ -131,8 +211,15 @@ export const Root = () => {
 					const right_edge = board_width - sprite_element_width;
 					const dead_end = tank_x >= right_edge;
 
-					if (dead_end) {
+					if (dead_end || canNotMove(code, tank_x, tank_y)) {
 						renderTank(sprite, { tank_x, tank_y });
+					}
+
+					if (canNotMove(code, tank_x, tank_y)) {
+						return {
+							tank_x: tank_x,
+							tank_y: tank_y
+						}	
 					}
 
 					return {
@@ -146,8 +233,15 @@ export const Root = () => {
 					const right_edge = board_width - sprite_element_width;
 					const dead_end = tank_y >= right_edge;
 
-					if (dead_end) {
+					if (dead_end || canNotMove(code, tank_x, tank_y)) {
 						renderTank(sprite, { tank_x, tank_y });
+					}
+
+					if (canNotMove(code, tank_x, tank_y)) {
+						return {
+							tank_x: tank_x,
+							tank_y: tank_y
+						}	
 					}
 
 					return {
@@ -160,8 +254,15 @@ export const Root = () => {
 				setTankCoordinates(({ tank_x, tank_y }) => {
 					const dead_end = tank_x <= 0;
 
-					if (dead_end) {
+					if (dead_end || canNotMove(code, tank_x, tank_y)) {
 						renderTank(sprite, { tank_x, tank_y });
+					}
+
+					if (canNotMove(code, tank_x, tank_y)) {
+						return {
+							tank_x: tank_x,
+							tank_y: tank_y
+						}	
 					}
 
 					return {
@@ -207,41 +308,76 @@ export const Root = () => {
 		handleTankImgCoordinates(last_pushed_key);
 
 		if (active_keys.current && active_keys.current.has('ArrowUp') && last_pushed_key === 'ArrowUp') {
-			setTankCoordinates(({ tank_x, tank_y }) => ({
-				tank_x: tank_x,
-				tank_y: tank_y <= 0 ? 0 : tank_y - 1
-			}));
+			setTankCoordinates(({ tank_x, tank_y }) => {
+				if (canNotMove(last_pushed_key, tank_x, tank_y)) {
+					return {
+						tank_x: tank_x,
+						tank_y: tank_y
+					}	
+				}
+				return {
+					tank_x: tank_x,
+					tank_y: tank_y <= 0 ? 0 : tank_y - 1
+				}
+			});
 		}
 
 		else if (active_keys.current && active_keys.current.has('ArrowRight') && last_pushed_key === 'ArrowRight') {
 			const right_edge = board_width - sprite_element_width;
 
-			setTankCoordinates(({ tank_x, tank_y }) => ({
-				tank_x: tank_x >= right_edge ? right_edge : tank_x + 1,
-				tank_y: tank_y
-			}));
+			setTankCoordinates(({ tank_x, tank_y }) => {
+				if (canNotMove(last_pushed_key, tank_x, tank_y)) {
+					return {
+						tank_x: tank_x,
+						tank_y: tank_y
+					}	
+				}
+				return {
+					tank_x: tank_x >= right_edge ? right_edge : tank_x + 1,
+					tank_y: tank_y
+				}
+			});
 		}
 
 		else if (active_keys.current && active_keys.current.has('ArrowDown') && last_pushed_key === 'ArrowDown') {
 			const bottom_edge = board_width - sprite_element_width;
 
-			setTankCoordinates(({ tank_x, tank_y }) => ({
-				tank_x: tank_x,
-				tank_y: tank_y >= bottom_edge ? bottom_edge : tank_y + 1
-			}));
+			setTankCoordinates(({ tank_x, tank_y }) => {
+				if (canNotMove(last_pushed_key, tank_x, tank_y)) {
+					return {
+						tank_x: tank_x,
+						tank_y: tank_y
+					}	
+				}
+				return {
+					tank_x: tank_x,
+					tank_y: tank_y >= bottom_edge ? bottom_edge : tank_y + 1
+				}
+			});
 		}
 
 		else if (active_keys.current && active_keys.current.has('ArrowLeft') && last_pushed_key === 'ArrowLeft') {
-			setTankCoordinates(({ tank_x, tank_y }) => ({
-				tank_x: tank_x <= 0 ? 0 : tank_x - 1,
-				tank_y: tank_y
-			}));
+			setTankCoordinates(({ tank_x, tank_y }) => {
+				if (canNotMove(last_pushed_key, tank_x, tank_y)) {
+					return {
+						tank_x: tank_x,
+						tank_y: tank_y
+					}	
+				}
+				return {
+					tank_x: tank_x <= 0 ? 0 : tank_x - 1,
+					tank_y: tank_y
+				}
+			});
 		}
 
 		current_animation_frame.current = requestAnimationFrame(loop);
 	};
 
-	const clearScreen = (context: CanvasRenderingContext2D) => {
+	const clearScreen = () => {
+		const canvas = canvas_ref.current;
+		const context = canvas.getContext('2d');
+
 		if (context) {
 			context.clearRect(0, 0, board_width, board_height);
 		}
@@ -258,7 +394,6 @@ export const Root = () => {
 		const { img_x, img_y } = tank_img_coordinates_ref.current;
 		const { step_x } = step_axis_ref.current;
 
-		clearScreen(context);
 		context.drawImage(
 				image,
 				img_x + sprite_element_width * step_x, img_y, sprite_element_width, sprite_element_width,
@@ -268,7 +403,8 @@ export const Root = () => {
 
 	useEffect(() => {
 		if (sprite.image.complete && sprite.image.naturalHeight !== 0) {
-			return renderTank(sprite, { tank_x, tank_y });
+			initGameboardLevels();
+			renderTank(sprite, { tank_x, tank_y });
 		}
 	}, [tank_x, tank_y]);
 
