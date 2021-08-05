fc718c5260f7612f42b02bc3f00e8cd56f0c3fad
slavon360 <slavon360@github.com>
2021-08-05 23:37:27 +0300

    grid data-structure + tank movement logic

--- src/components/root/root.tsx
+++ src/components/root/root.tsx
@@ -5,7 +5,8 @@ import {
 	board_height,
 	sprite_element_width,
 	GRID_ELEMENTS_LEVEL1,
-	SPRITES_ELEMENTS
+	TYPES
+	// SPRITES_ELEMENTS
 } from '../../utils/common-data';
 // import canvasContext from '../../context/canvas-context';
 import Sprite from '../../utils/sprite';
@@ -15,6 +16,17 @@ import './gameboard.css';
 
 interface activeKeys {
 	current: null | Set<string>
+}
+
+interface debug {
+	current: boolean
+}
+
+interface debugCoordinates {
+	x_begin: number,
+	width: number,
+	y_begin: number,
+	height: number
 }
 
 interface imgCoordinates {
@@ -45,6 +57,7 @@ export const Root = () => {
 	const canvas_ref: any = useRef(null);
 	const tank_img_coordinates_ref: imgCoordinatesRef = useRef({ img_x: 0, img_y: 0 });
 	const step_axis_ref: stepAxisRef = useRef({ step_x: 0, step_y: 0 });
+	const is_debug_ref: debug = useRef(false);
 	// const flattened_grid_ref: any = useRef(null);
 	
 	const initGameboardLevels = useCallback(() => {
@@ -54,21 +67,87 @@ export const Root = () => {
 
 		GRID_ELEMENTS_LEVEL1.forEach((row, row_index) => {
 			row.forEach((cell, cell_index) => {
-				const [x, y, width, height] = SPRITES_ELEMENTS[cell];
+				const {
+					sprite_x,
+					sprite_y,
+					sprite_width,
+					sprite_height
+				} = cell.type;
 				context.drawImage(
 					image,
-					x, y, width, height,
-					cell_index * sprite_element_width, row_index * sprite_element_width, width, height
+					sprite_x, sprite_y, sprite_width, sprite_height,
+					cell_index * sprite_element_width, row_index * sprite_element_width, sprite_width, sprite_height
 				);
 			});
 		})
 	}, []);
 
+	const is_tank_on_the_same_axis_level_with_object = (type_name: string, tank_axis: number, element_axis: number) => {
+		return type_name !== TYPES.BLANK && tank_axis === element_axis;
+	}
+	const is_tank_opposite_to_object_at_y_axis = (
+			tank_axis_begin: number,
+			tank_axis_end: number,
+			element_axis_begin: number,
+			element_axis_end: number,
+			sprite_element_width: number
+		) => {
+		return (tank_axis_begin >= element_axis_begin && tank_axis_end >= element_axis_end &&
+			tank_axis_begin - element_axis_begin < sprite_element_width) ||
+			(tank_axis_begin <= element_axis_begin && tank_axis_end <= element_axis_end &&
+			element_axis_end - tank_axis_end < sprite_element_width);
+	}
+
+	const is_tank_opposite_to_object_at_x_axis = (
+		tank_axis_begin: number,
+		tank_axis_end: number,
+		element_axis_begin: number,
+		element_axis_end: number,
+		sprite_element_width: number
+	) => {
+		return (tank_axis_end >= element_axis_end &&
+			tank_axis_end - element_axis_end >= 0 && tank_axis_end - element_axis_end < sprite_element_width) ||
+			(tank_axis_end <= element_axis_end &&
+			(tank_axis_begin - element_axis_begin >= 0 && tank_axis_begin - element_axis_begin < sprite_element_width) ||
+			element_axis_begin - tank_axis_begin  >= 0 && element_axis_begin - tank_axis_begin < sprite_element_width);
+	}
+
+	const handle_tank_axis_adjustment = (
+		tank_axis_begin: number,
+		tank_axis_end: number,
+		element_axis_begin: number,
+		element_axis_end: number,
+		sprite_element_width: number
+	) => {
+		const difference1 = element_axis_end - tank_axis_begin;
+		const difference2 = tank_axis_end - element_axis_begin;
+		const should_adjust = (difference1 <= sprite_element_width / 5) || (difference2 <= sprite_element_width / 5);
+		const adjusted_value = difference1 > difference2 ? -difference2 : difference1;
+
+		return should_adjust ? adjusted_value : 0;
+	}
+
+	const debugHandler = (coordinates: debugCoordinates) => {
+		const {
+			x_begin,
+			width,
+			y_begin,
+			height
+		} = coordinates;
+		const canvas = canvas_ref.current;
+		const context = canvas.getContext('2d');
+
+		context.strokeStyle = '#ffffff';
+		context.lineWidth = 1;
+		context.strokeRect(x_begin, y_begin, width, height);
+	};
+
 	const canNotMove = (direction: string, tank_x: number, tank_y: number ) => {
-		// let cant = false;
-		return GRID_ELEMENTS_LEVEL1.find((row, row_index) => {
+		let adjusted_value = 0;
+
+		const cant_move = GRID_ELEMENTS_LEVEL1.find((row, row_index) => {
 			return row.find((cell, cell_index) => {
-				const [sprite_x, sprite_y, sprite_width, sprite_height] = SPRITES_ELEMENTS[cell];
+				const { sprite_width, sprite_height, type_name } = cell.type;
 				const element_x_begin = cell_index * sprite_width;
 				const element_x_end = element_x_begin + sprite_width;
 				const element_y_begin = row_index * sprite_height;
@@ -79,53 +158,90 @@ export const Root = () => {
 				switch (direction) {
 					case 'ArrowUp':
 						if (
-							cell > 0 && tank_y === element_y_end &&
-							(tank_x >= element_x_begin && tank_x_end >= element_x_end &&
-							tank_x - element_x_begin < sprite_element_width) ||
-							cell > 0 && tank_y === element_y_end &&
-							(tank_x <= element_x_begin && tank_x_end <= element_x_end &&
-							element_x_end - tank_x_end < sprite_element_width) 
+							is_tank_on_the_same_axis_level_with_object(type_name, tank_y, element_y_end) &&
+							is_tank_opposite_to_object_at_y_axis(tank_x, tank_x_end, element_x_begin, element_x_end, sprite_element_width)
 						) {
-							console.log(tank_x, element_x_begin, tank_x_end, element_x_end);
+							if (is_debug_ref.current) {
+								debugHandler({
+									x_begin: element_x_begin,
+									width: sprite_width,
+									y_begin: element_y_begin,
+									height: sprite_height
+								});
+							}
+							adjusted_value = handle_tank_axis_adjustment(tank_x, tank_x_end, element_x_begin, element_x_end, sprite_element_width);
+
 							return true;
+						} else if (is_tank_opposite_to_object_at_y_axis(tank_x, tank_x_end, element_x_begin, element_x_end, sprite_element_width)) {
+							adjusted_value = adjusted_value || handle_tank_axis_adjustment(tank_x, tank_x_end, element_x_begin, element_x_end, sprite_element_width);
+
+							return false;
 						}
 						return false;
 					case 'ArrowDown':
 						if (
-							cell > 0 && tank_y_end === element_y_begin &&
-							(tank_x >= element_x_begin && tank_x_end >= element_x_end &&
-							tank_x - element_x_begin < sprite_element_width) ||
-							cell > 0 && tank_y_end === element_y_begin &&
-							(tank_x <= element_x_begin && tank_x_end <= element_x_end &&
-							element_x_end - tank_x_end < sprite_element_width) 
+							is_tank_on_the_same_axis_level_with_object(type_name, tank_y_end, element_y_begin) &&
+							is_tank_opposite_to_object_at_y_axis(tank_x, tank_x_end, element_x_begin, element_x_end, sprite_element_width)
 						) {
+							if (is_debug_ref.current) {
+								debugHandler({
+									x_begin: element_x_begin,
+									width: sprite_width,
+									y_begin: element_y_begin,
+									height: sprite_height
+								});
+							}
+							adjusted_value = handle_tank_axis_adjustment(tank_x, tank_x_end, element_x_begin, element_x_end, sprite_element_width);
+
 							return true;
+						} else if (is_tank_opposite_to_object_at_y_axis(tank_x, tank_x_end, element_x_begin, element_x_end, sprite_element_width)) {
+							adjusted_value = adjusted_value || handle_tank_axis_adjustment(tank_x, tank_x_end, element_x_begin, element_x_end, sprite_element_width);
+
+							return false;
 						}
 						return false;
 					case 'ArrowLeft':
 						if (
-							cell > 0 && tank_x === element_x_end &&
-							(tank_y_end >= element_y_end &&
-							tank_y_end - element_y_end >= 0 && tank_y_end - element_y_end < sprite_element_width) ||
-							cell > 0 && tank_x === element_x_end &&
-							(tank_y_end <= element_y_end &&
-							(tank_y - element_y_begin >= 0 && tank_y - element_y_begin < sprite_element_width) ||
-							element_y_begin - tank_y  >= 0 && element_y_begin - tank_y < sprite_element_width)
+							is_tank_on_the_same_axis_level_with_object(type_name, tank_x, element_x_end) &&
+							is_tank_opposite_to_object_at_x_axis(tank_y, tank_y_end, element_y_begin, element_y_end, sprite_element_width)
 						) {
+							if (is_debug_ref.current) {
+								debugHandler({
+									x_begin: element_x_begin,
+									width: sprite_width,
+									y_begin: element_y_begin,
+									height: sprite_height
+								});
+							}
+							adjusted_value = handle_tank_axis_adjustment(tank_y, tank_y_end, element_y_begin, element_y_end, sprite_element_width);
+
 							return true;
+						} else if (is_tank_opposite_to_object_at_x_axis(tank_y, tank_y_end, element_y_begin, element_y_end, sprite_element_width)) {
+							adjusted_value = adjusted_value || handle_tank_axis_adjustment(tank_y, tank_y_end, element_y_begin, element_y_end, sprite_element_width);
+
+							return false;
 						}
 						return false;
 					case 'ArrowRight':
 						if (
-							cell > 0 && tank_x_end === element_x_begin &&
-							(tank_y_end >= element_y_end &&
-							tank_y_end - element_y_end >= 0 && tank_y_end - element_y_end < sprite_element_width) ||
-							cell > 0 && tank_x_end === element_x_begin &&
-							(tank_y_end <= element_y_end &&
-							(tank_y - element_y_begin >= 0 && tank_y - element_y_begin < sprite_element_width) ||
-							element_y_begin - tank_y  >= 0 && element_y_begin - tank_y < sprite_element_width)
+							is_tank_on_the_same_axis_level_with_object(type_name, tank_x_end, element_x_begin) &&
+							is_tank_opposite_to_object_at_x_axis(tank_y, tank_y_end, element_y_begin, element_y_end, sprite_element_width)
 						) {
+							if (is_debug_ref.current) {
+								debugHandler({
+									x_begin: element_x_begin,
+									width: sprite_width,
+									y_begin: element_y_begin,
+									height: sprite_height
+								});
+							}
+							adjusted_value = handle_tank_axis_adjustment(tank_y, tank_y_end, element_y_begin, element_y_end, sprite_element_width);
+
 							return true;
+						} else if (is_tank_opposite_to_object_at_x_axis(tank_y, tank_y_end, element_y_begin, element_y_end, sprite_element_width)) {
+							adjusted_value = adjusted_value || handle_tank_axis_adjustment(tank_y, tank_y_end, element_y_begin, element_y_end, sprite_element_width);
+
+							return false;
 						}
 						return false;
 
@@ -133,8 +249,8 @@ export const Root = () => {
 				}
 			});
 		});
-		// console.log(cant);
-		// return cant;
+
+		return { cant_move, adjusted_value };
 	}
 
 	const handleTankImgCoordinates = (key: string) => {
@@ -174,7 +290,6 @@ export const Root = () => {
 	}
 
 	const handleKeyDownPress = useCallback(event => {
-		event.preventDefault();
 		const { code } = event;
 
 		if (active_keys.current) {
@@ -188,98 +303,119 @@ export const Root = () => {
 			case 'ArrowUp':
 				setTankCoordinates(({ tank_x, tank_y }) => {
 					const dead_end = tank_y <= 0;
+					const { cant_move, adjusted_value } = canNotMove(code, tank_x, tank_y);
 
-					if (dead_end || canNotMove(code, tank_x, tank_y)) {
+					if (dead_end || cant_move) {
 						renderTank(sprite, { tank_x, tank_y });
-					}
-
-					if (canNotMove(code, tank_x, tank_y)) {
 						return {
-							tank_x: tank_x,
+							tank_x: tank_x + adjusted_value,
 							tank_y: tank_y
 						}	
 					}
 
 					return {
-						tank_x: tank_x,
+						tank_x: tank_x + adjusted_value,
 						tank_y: dead_end ? 0 : tank_y - 1
 					}
 				});
+				event.preventDefault();
 				break;
 			case 'ArrowRight':
 				setTankCoordinates(({ tank_x, tank_y }) => {
 					const right_edge = board_width - sprite_element_width;
 					const dead_end = tank_x >= right_edge;
+					const { cant_move, adjusted_value } = canNotMove(code, tank_x, tank_y);
 
-					if (dead_end || canNotMove(code, tank_x, tank_y)) {
+					if (dead_end || cant_move) {
 						renderTank(sprite, { tank_x, tank_y });
-					}
 
-					if (canNotMove(code, tank_x, tank_y)) {
 						return {
 							tank_x: tank_x,
-							tank_y: tank_y
-						}	
+							tank_y: tank_y + adjusted_value
+						}
 					}
+
+					// if (canNotMove(code, tank_x, tank_y)) {
+					// 	return {
+					// 		tank_x: tank_x,
+					// 		tank_y: tank_y
+					// 	}
+					// }
 
 					return {
 						tank_x: dead_end ? right_edge : tank_x + 1,
-						tank_y: tank_y
+						tank_y: tank_y + adjusted_value
 					}
 				});
+				event.preventDefault();
 				break;
 			case 'ArrowDown':
 				setTankCoordinates(({ tank_x, tank_y }) => {
 					const right_edge = board_width - sprite_element_width;
 					const dead_end = tank_y >= right_edge;
+					const { cant_move, adjusted_value } = canNotMove(code, tank_x, tank_y);
 
-					if (dead_end || canNotMove(code, tank_x, tank_y)) {
+					if (dead_end || cant_move) {
 						renderTank(sprite, { tank_x, tank_y });
+
+						return {
+							tank_x: tank_x + adjusted_value,
+							tank_y: tank_y
+						}
 					}
 
-					if (canNotMove(code, tank_x, tank_y)) {
-						return {
-							tank_x: tank_x,
-							tank_y: tank_y
-						}	
-					}
+					// if (canNotMove(code, tank_x, tank_y)) {
+					// 	return {
+					// 		tank_x: tank_x,
+					// 		tank_y: tank_y
+					// 	}
+					// }
 
 					return {
-						tank_x: tank_x,
+						tank_x: tank_x + adjusted_value,
 						tank_y: dead_end ? right_edge : tank_y + 1
 					};
 				});
+				event.preventDefault();
 				break;
 			case 'ArrowLeft':
 				setTankCoordinates(({ tank_x, tank_y }) => {
 					const dead_end = tank_x <= 0;
+					const { cant_move, adjusted_value } = canNotMove(code, tank_x, tank_y);
 
-					if (dead_end || canNotMove(code, tank_x, tank_y)) {
+					if (dead_end || cant_move) {
 						renderTank(sprite, { tank_x, tank_y });
-					}
 
-					if (canNotMove(code, tank_x, tank_y)) {
 						return {
 							tank_x: tank_x,
-							tank_y: tank_y
-						}	
+							tank_y: tank_y + adjusted_value
+						}
 					}
+
+					// if (canNotMove(code, tank_x, tank_y)) {
+					// 	return {
+					// 		tank_x: tank_x,
+					// 		tank_y: tank_y
+					// 	}
+					// }
 
 					return {
 						tank_x: dead_end ? 0 : tank_x - 1,
-						tank_y: tank_y
+						tank_y: tank_y + adjusted_value
 					}
 				});
+				event.preventDefault();
 				break;
 			case 'Space':
+				event.preventDefault();
 				break;
 			case 'Enter':
+				event.preventDefault();
 				break;
 		}
 	}, []);
 
 	const handleKeyUpPress = useCallback(event => {
-		event.preventDefault();
 		const { code } = event;
 
 		if (active_keys.current) {
@@ -291,10 +427,9 @@ export const Root = () => {
 			case 'ArrowRight':
 			case 'ArrowDown':
 			case 'ArrowLeft':
-				break;
 			case 'Space':
-				break;
 			case 'Enter':
+				event.preventDefault();
 				break;
 		}
 	}, []);
@@ -309,14 +444,17 @@ export const Root = () => {
 
 		if (active_keys.current && active_keys.current.has('ArrowUp') && last_pushed_key === 'ArrowUp') {
 			setTankCoordinates(({ tank_x, tank_y }) => {
-				if (canNotMove(last_pushed_key, tank_x, tank_y)) {
+				const { cant_move, adjusted_value } = canNotMove(last_pushed_key, tank_x, tank_y);
+
+				if (cant_move) {
 					return {
-						tank_x: tank_x,
+						tank_x: tank_x + adjusted_value,
 						tank_y: tank_y
 					}	
 				}
+
 				return {
-					tank_x: tank_x,
+					tank_x: tank_x + adjusted_value,
 					tank_y: tank_y <= 0 ? 0 : tank_y - 1
 				}
 			});
@@ -326,15 +464,17 @@ export const Root = () => {
 			const right_edge = board_width - sprite_element_width;
 
 			setTankCoordinates(({ tank_x, tank_y }) => {
-				if (canNotMove(last_pushed_key, tank_x, tank_y)) {
+				const { cant_move, adjusted_value } = canNotMove(last_pushed_key, tank_x, tank_y);
+
+				if (cant_move) {
 					return {
 						tank_x: tank_x,
-						tank_y: tank_y
+						tank_y: tank_y + adjusted_value
 					}	
 				}
 				return {
 					tank_x: tank_x >= right_edge ? right_edge : tank_x + 1,
-					tank_y: tank_y
+					tank_y: tank_y + adjusted_value
 				}
 			});
 		}
@@ -343,14 +483,16 @@ export const Root = () => {
 			const bottom_edge = board_width - sprite_element_width;
 
 			setTankCoordinates(({ tank_x, tank_y }) => {
-				if (canNotMove(last_pushed_key, tank_x, tank_y)) {
+				const { cant_move, adjusted_value } = canNotMove(last_pushed_key, tank_x, tank_y);
+
+				if (cant_move) {
 					return {
-						tank_x: tank_x,
+						tank_x: tank_x + adjusted_value,
 						tank_y: tank_y
 					}	
 				}
 				return {
-					tank_x: tank_x,
+					tank_x: tank_x + adjusted_value,
 					tank_y: tank_y >= bottom_edge ? bottom_edge : tank_y + 1
 				}
 			});
@@ -358,15 +500,17 @@ export const Root = () => {
 
 		else if (active_keys.current && active_keys.current.has('ArrowLeft') && last_pushed_key === 'ArrowLeft') {
 			setTankCoordinates(({ tank_x, tank_y }) => {
-				if (canNotMove(last_pushed_key, tank_x, tank_y)) {
+				const { cant_move, adjusted_value } = canNotMove(last_pushed_key, tank_x, tank_y);
+
+				if (cant_move) {
 					return {
 						tank_x: tank_x,
-						tank_y: tank_y
+						tank_y: tank_y + adjusted_value
 					}	
 				}
 				return {
 					tank_x: tank_x <= 0 ? 0 : tank_x - 1,
-					tank_y: tank_y
+					tank_y: tank_y + adjusted_value
 				}
 			});
 		}
--- src/utils/common-data.ts
+++ src/utils/common-data.ts
@@ -1,8 +1,31 @@
 export const sprite_element_width = 16;
+export const TYPES = {
+	BLANK: 'BLANK',
+	FORTRESS: 'FORTRESS',
+	BRICK: 'BRICK'
+};
 export const OBJECT_TYPE = {
-	BLANK: [336, 0, sprite_element_width, sprite_element_width],
-	FORTRESS: [19 * sprite_element_width, 2 * sprite_element_width, sprite_element_width, sprite_element_width],
-	BRICK: [sprite_element_width * sprite_element_width, 0, sprite_element_width, sprite_element_width]
+	BLANK: {
+		sprite_x: 336,
+		sprite_y: 0,
+		sprite_width: sprite_element_width,
+		sprite_height: sprite_element_width,
+		type_name: TYPES.BLANK
+	},
+	FORTRESS: {
+		sprite_x: 19 * sprite_element_width,
+		sprite_y: 2 * sprite_element_width,
+		sprite_width: sprite_element_width,
+		sprite_height: sprite_element_width,
+		type_name: TYPES.FORTRESS
+	},
+	BRICK: {
+		sprite_x: sprite_element_width * sprite_element_width,
+		sprite_y: 0,
+		sprite_width: sprite_element_width,
+		sprite_height: sprite_element_width,
+		type_name: TYPES.BRICK
+	}
 };
 
 export const SPRITES_ELEMENTS = [
@@ -17,18 +40,611 @@ export const board_height = sprite_element_width * 13;
 export const GRID_SIZE = 20;
 export const CELL_SIZE = 20;
 
+const DAMAGE_LEVEL = {
+	INTACT: 'INTACT',
+	TOP1X: 'TOP1X',
+	TOP2X: 'TOP2X',
+	TOP3X: 'TOP3X',
+	BOTTOM1X: 'BOTTOM1X',
+	BOTTOM2X: 'BOTTOM2X',
+	BOTTOM3X: 'BOTTOM3X',
+	LEFT1X: 'LEFT1X',
+	LEFT2X: 'LEFT2X',
+	LEFT3X: 'LEFT3X',
+	RIGHT1X: 'RIGHT1X',
+	RIGHT2X: 'RIGHT2X',
+	RIGHT3X: 'RIGHT3X',
+	TOP_LEFT1X: 'TOP_LEFT1X',
+	TOP_LEFT2X: 'TOP_LEFT2X',
+	TOP_LEFT3X: 'TOP_LEFT3X',
+	TOP_RIGHT1X: 'TOP_RIGHT1X',
+	TOP_RIGHT2X: 'TOP_RIGHT2X',
+	TOP_RIGHT3X: 'TOP_RIGHT3X',
+	BOTTOM_LEFT1X: 'BOTTOM_LEFT1X',
+	BOTTOM_LEFT2X: 'BOTTOM_LEFT2X',
+	BOTTOM_LEFT3X: 'BOTTOM_LEFT3X',
+	BOTTOM_RIGHT1X: 'BOTTOM_RIGHT1X',
+	BOTTOM_RIGHT2X: 'BOTTOM_RIGHT2X',
+	BOTTOM_RIGHT3X: 'BOTTOM_RIGHT3X',
+	LEFT_TOP1X: 'LEFT_TOP1X',
+	LEFT_TOP2X: 'LEFT_TOP2X',
+	LEFT_TOP3X: 'LEFT_TOP3X',
+	LEFT_BOTTOM1X: 'LEFT_BOTTOM1X',
+	LEFT_BOTTOM2X: 'LEFT_BOTTOM2X',
+	LEFT_BOTTOM3X: 'LEFT_BOTTOM3X',
+	RIGHT_TOP1X: 'RIGHT_TOP1X',
+	RIGHT_TOP2X: 'RIGHT_TOP2X',
+	RIGHT_TOP3X: 'RIGHT_TOP3X',
+	RIGHT_BOTTOM1X: 'RIGHT_BOTTOM1X',
+	RIGHT_BOTTOM2X: 'RIGHT_BOTTOM2X',
+	RIGHT_BOTTOM3X: 'RIGHT_BOTTOM3X'
+}
+
 export const GRID_ELEMENTS_LEVEL1 = [
-	[ 2, 0, 0, 0, 2, 2, 2, 2, 2, 2, 0, 0, 0 ],
-	[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ], 
-	[ 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0 ], 
-	[ 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0 ], 
-	[ 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0 ], 
-	[ 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0 ], 
-	[ 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2 ], 
-	[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ], 
-	[ 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0 ], 
-	[ 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0 ], 
-	[ 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0 ], 
-	[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0 ], 
-	[ 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 ]
+	[ 
+		{
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		}
+	],
+	[ 
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}
+	], 
+	[
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}
+	], 
+	[
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		}
+	],
+	[
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		}
+	],
+	[
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}, 
+		{
+			type: OBJECT_TYPE.BLANK
+		}
+	],
+	[
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		}, 
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		}, 
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		}, 
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		}
+	],
+	[
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}
+	],
+	[
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}
+	],
+	[
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}
+	],
+	[
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}
+	],
+	[
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{ 
+			type: OBJECT_TYPE.BRICK,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}
+	],
+	[
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{ 
+			type: OBJECT_TYPE.FORTRESS,
+			damage: DAMAGE_LEVEL.INTACT
+		},
+		{ 
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		},
+		{
+			type: OBJECT_TYPE.BLANK
+		}
+	]
 ]
